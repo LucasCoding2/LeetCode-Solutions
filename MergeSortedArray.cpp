2ab0a1e1bb42f22ab2e8103c9117267aa4c9eb94
LucasCoding2 <152044928+LucasCoding2@users.noreply.github.com>
2026-03-03 11:54:06 -0800

    Create MergeSortedArray.cpp

---
+++ MergeSortedArray.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
+       int i = 0;
+       int j = 0;
+       vector<int> nums3;
+       while(i < m || j < n) {
+           if(i >= m || (j < n && nums2[j] <= nums1[i]) ) {
+               nums3.push_back(nums2[j]);
+               j++;
+           }
+           else {
+               nums3.push_back(nums1[i]);
+               i++;
+           }
+       } 
+       nums1 = nums3;
+    }
+};
