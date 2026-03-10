aab218a3c1d32c520ca80b9e6c81db480524c52b
LucasCoding2 <152044928+LucasCoding2@users.noreply.github.com>
2026-03-10 11:32:00 -0700

    Update LongestCommonPrefix.cpp
    New solution found with help

--- LongestCommonPrefix.cpp
+++ LongestCommonPrefix.cpp
@@ -1,24 +1,16 @@
 class Solution {
 public:
-    int strStr(string haystack, string needle) {
-        int index = -1;
-        if(needle.length() > haystack.length() ) {
-            return -1;
-        }
-        for(int i = 0; i < haystack.length()-needle.length()+1;i++) {
-            if(haystack[i] == needle[0]) {
-                index = i;
-            }
-            int j;
-            for(j = 0; j < needle.length(); j++) {
-                if(haystack[i+j] != needle[j]) {
-                    index = -1;
+    string longestCommonPrefix(vector<string>& strs) {
+        for(int i = 0; i < strs[0].length(); i++) {
+            char c = strs[0][i];
+
+            for(int j = 1; j < strs.size(); j++) {
+                if(strs[j][i] != c) {
+                    return strs[0].substr(0, i);
                 }
             }
-            if(index != -1) {
-                return index;
-            }
         }
-        return index;
+
+        return strs[0];
     }
 };
