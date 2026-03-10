1721fc7dcf87148f1d6b11e1fbea3012d3daad31
LucasCoding2 <152044928+LucasCoding2@users.noreply.github.com>
2026-03-10 10:56:58 -0700

    Update LongestCommonPrefix.cpp

--- LongestCommonPrefix.cpp
+++ LongestCommonPrefix.cpp
@@ -1,17 +1,27 @@
 class Solution {
 public:
-    string longestCommonPrefix(vector<string>& strs) {
-       string longestCommonPrefix = "";
-       for(int i = 1; i < strs[0].length(); i++) {
-           int j = 0;
-           while(j < strs.size() && strs[0].substr(0,i) == strs[j].substr(0,i) ) {
-             j++;
-           }
-           if(j == strs.size() ) {
-               longestCommonPrefix = longestCommonPrefix + strs[0][i-1];
-           }      
-       }
-       
-       return longestCommonPrefix;
+    int strStr(string haystack, string needle) {
+        int index = -1;
+        if(needle.length() > haystack.length() ) {
+            return -1;
+        }
+        if(haystack.length() == 1 && needle.length() == 1
+        && haystack[0] == needle[0]) {
+            return 0;
+        }
+        for(int i = 0; i < haystack.length()-needle.length()+1;i++) {
+            if(haystack[i] == needle[0]) {
+                index = i;
+            }            
+            for(int j = 0; j < needle.length(); j++) {
+                if(haystack[i+j] != needle[j]) {
+                    index = -1;
+                }
+            }
+            if(index != -1) {
+                return index;
+            }
+        }
+        return index;
     }
 };
