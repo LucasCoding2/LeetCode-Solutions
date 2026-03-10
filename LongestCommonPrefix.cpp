7d68da903c3a42a4f06a0d8058e737a841f2c159
LucasCoding2 <152044928+LucasCoding2@users.noreply.github.com>
2026-03-10 10:57:50 -0700

    Update LongestCommonPrefix.cpp

--- LongestCommonPrefix.cpp
+++ LongestCommonPrefix.cpp
@@ -5,15 +5,12 @@ public:
         if(needle.length() > haystack.length() ) {
             return -1;
         }
-        if(haystack.length() == 1 && needle.length() == 1
-        && haystack[0] == needle[0]) {
-            return 0;
-        }
         for(int i = 0; i < haystack.length()-needle.length()+1;i++) {
             if(haystack[i] == needle[0]) {
                 index = i;
-            }            
-            for(int j = 0; j < needle.length(); j++) {
+            }
+            int j;
+            for(j = 0; j < needle.length(); j++) {
                 if(haystack[i+j] != needle[j]) {
                     index = -1;
                 }
