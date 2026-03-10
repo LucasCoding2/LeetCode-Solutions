31bb8ca81a067fa453697be967effe687fb2e6e2
LucasCoding2 <152044928+LucasCoding2@users.noreply.github.com>
2026-03-10 10:31:46 -0700

    Update TwoSum.cpp
    Fixed (did not originally submit to check code)

--- TwoSum.cpp
+++ TwoSum.cpp
@@ -3,11 +3,11 @@ public:
     vector<int> twoSum(vector<int>& nums, int target) {
       for(int i = 0; i < nums.size()-1; i++) {
         for(int j = 1; j < nums.size(); j++) {
-            if(nums[i]+nums[j] == target) {
-                vector<int> indices;
-                indices.push_back(i);
-                indices.push_back(j);
-                return indices;
+            if(i != j && nums[i]+nums[j] == target) {
+                vector<int> operands;
+                operands.push_back(i);
+                operands.push_back(j);
+                return operands;
             }
         }
       }  
