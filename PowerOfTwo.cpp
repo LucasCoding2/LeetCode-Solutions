6fe9c22aa98abab8d703dec5e2124f6ad0cb3680
LucasCoding2 <152044928+LucasCoding2@users.noreply.github.com>
2026-03-22 23:44:47 -0700

    Update PowerOfTwo.cpp
    This is one of the solutions from leetcode. Im trying to understand it  and will make an explanation of why it works later.
     Just for now, I know that n-1 leaves ones behind and at least one of those ones will be the same spot as one of the ones from n if n is not a power of two.

--- PowerOfTwo.cpp
+++ PowerOfTwo.cpp
@@ -1,13 +1,6 @@
 class Solution {
 public:
     bool isPowerOfTwo(int n) {
-        if(n <= 0) {
-            return false;
-        }
-        double num = log2(n);
-        if(num > floor(num) && num < ceil(num)) {
-            return false;
-        }
-        return true;
+        return n > 0 && not (n & n - 1);
     }
 };
