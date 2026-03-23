cd8cb16a264fc5756e4977e81130ef61709e5e3d
LucasCoding2 <152044928+LucasCoding2@users.noreply.github.com>
2026-03-22 22:21:59 -0700

    Create RemoveDuplicatesFromSortedList.cpp

---
+++ RemoveDuplicatesFromSortedList.cpp
@@ -0,0 +1,30 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* deleteDuplicates(ListNode* head) {
+        if(head == NULL) {
+            return head;
+        }
+        ListNode* node = head;
+        int val = node->val;
+        while(node->next != NULL) {
+            if(val == node->next->val) {
+                node->next = node->next->next; 
+            }
+            else {
+                node = node->next;
+                val = node->val;
+            }
+        }
+        return head;
+    }
+};
