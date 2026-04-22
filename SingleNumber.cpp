c5d8f7587a4cf8372110c15227429157a1e227d8
LucasCoding2 <152044928+LucasCoding2@users.noreply.github.com>
2026-04-21 23:17:38 -0700

    Update SingleNumber.cpp
    With Chat GPT help

--- SingleNumber.cpp
+++ SingleNumber.cpp
@@ -1,15 +1,10 @@
 class Solution {
 public:
     int singleNumber(vector<int>& nums) {
-       std::unordered_map<int, int> numsMap;
-       for(int i = 0; i < nums.size(); i++) {
-           numsMap[nums[i]] += 1;
-       } 
-       for (const auto& [key, value] : numsMap) {
-           if(value < 2) {
-               return key;
-           }
-       }
-       return -1;
+        int result = 0;
+        for (int num : nums) {
+            result ^= num;
+        }
+        return result;
     }
 };
