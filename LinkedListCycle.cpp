86f3b2bac3c5cc9ee5da99c6f081b768ff0df507
LucasCoding2 <152044928+LucasCoding2@users.noreply.github.com>
2026-08-21 13:11:35 -0700

    Create LinkedListCycle.cpp
    I realize there is a better solution I will come back to this one later

---
+++ LinkedListCycle.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    bool hasCycle(ListNode *head) {
+        unordered_set<ListNode*> visited;
+        ListNode* curr = head;
+
+        while (curr != nullptr) {
+            if (visited.count(curr)) {
+                return true;
+            }
+            visited.insert(curr);
+            curr = curr->next;
+        }
+        return false;
+    }
+};
